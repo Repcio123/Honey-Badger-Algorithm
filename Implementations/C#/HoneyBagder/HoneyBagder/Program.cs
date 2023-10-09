b46101e30f168a1859cc0896a84d567594d66adc
BarRep <ketrabceper@gmail.com>
2023-10-09 12:44:33 +0200

    Update Program.cs

--- Implementations/C#/HoneyBagder/HoneyBagder/Program.cs
+++ Implementations/C#/HoneyBagder/HoneyBagder/Program.cs
@@ -5,42 +5,51 @@ namespace HoneyBadger
 {
     internal class Program
     {
-        private const int dim = 7;
-        private const int n = 7;
-        static double[,] SetUpPosition(int n,
-            int dim, double ub, double lb)
+        private const int Dimensions = 7;
+        private const int Population = 7;
+        private static readonly double Epsilon = Math.Pow(2, -52); // smallest possible number in C#
+
+        static private double FitnessFunction(double x) => Math.Abs(x);
+
+
+        static private double[,] SetUpPosition(int population,
+            int dimensions,
+            double upperBound,
+            double lowerBound)
         {
-            var x  = new double[dim,n];
-            for (int i = 0; i < dim; i++)
+            var x  = new double[dimensions, population];
+            for (int i = 0; i < dimensions; i++)
             {
-                for(int j = 0; j < n; j++)
+                for(int j = 0; j < population; j++)
                 {
                     var rand = new Random();
-                    double r1 = rand.NextDouble()*2-1;
-                    x[i,j] = lb + r1 * (lb-ub);
-                    
+                    double r1 = rand.NextDouble() * 2 - 1;
+                    x[i,j] = lowerBound + r1 * (lowerBound-upperBound);   
                 }
-                
             }
             return x;
         }
-        static List<double> HoneyBadgerAlgorithm(int n,
-            int tmax, double lb, double up, int dim, double c, double b)
-        {
 
-            return new List<double> { };
+        static private double[] CreateAgentsFitness(double[,] x, int population, int dimensions)
+        {
+            f = new double[population];
+        }
+
+        static private double[,] HoneyBadgerAlgorithm(int population,
+            int iterations,
+            double lowerBound,
+            double upperBound,
+            int dimensions,
+            double c,
+            double b)
+        {
+            var positions = SetUpPosition(population, dimensions,  upperBound, lowerBound);
+
+            return positions;
         }
         static void Main(string[] args)
         {
-           var d =  SetUpPosition(n,dim,-3.5,3.5);
-            for (int i = 0;i < dim;i++)
-            {
-                for(int j= 0; j < n; j++)
-                {
-                    Console.Write(d[i,j].ToString().Substring(0,5) + " ");
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine(Epsilon);
         }
     }
 }
